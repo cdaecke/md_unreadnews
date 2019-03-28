a472b27d4af2869ecd9e927dd646d58750ab3232
chris <typo3@mediadreams.org>
2019-03-28 12:32:19 +0100

    [TASK] don't register plugin in backend

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -9,11 +9,14 @@ call_user_func(
          * Register plugin
          *
          */
+        /*
+        // do not register the plugin, but load it via typoscript
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             'Mediadreams.MdUnreadnews',
             'Unread',
             'Unread News'
         );
+        */
 
     }
 );
